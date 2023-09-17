3c1193c24660bc26e92c12d03063975ec9b9e040
juratbek8581 <133354502+juratbek8581@users.noreply.github.com>
2023-09-17 22:37:07 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATIC_ROOT = '/home/user/collectedstatic/'
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 
 
